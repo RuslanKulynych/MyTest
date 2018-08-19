89a98e0ae6aa3c232ae45f18b4e2b3fca452c3f2
Ruslan Kulynych <ruslan.kulynych8@gmail.com>
2018-08-19 22:56:47 +0300

    number of digits after 0 has been fixed

--- FE_9_6_homework_js-intro/homework/src/js/task2.js
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -5,6 +5,11 @@ var num180 = 180;
 function degToRad (angle) {
 	return angle / num180 * Math.PI; 
 }
+
+function roundToTwoDigitsAfterComma(floatNumber) {
+    return parseFloat((Math.round(floatNumber * 100) / 100).toFixed(2));
+}
+
 var sideC = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2) - 2 * sideA * sideB * Math.cos(degToRad(angle)));
 
 var perimeter = Number(sideA) + Number(sideB) + Number(sideC);
@@ -13,29 +18,5 @@ var pHalf = perimeter/2;
 
 var S = Math.sqrt(pHalf * (pHalf - sideA) * (pHalf - sideB) * (pHalf - sideC));
 
-if (sideC === parseInt(sideC)) {
-	sideC = sideC.toFixed();
-} else {
-	sideC = sideC.toFixed(2);
-}
 
-if (perimeter === parseInt(perimeter)) {
-	perimeter = perimeter.toFixed();
-} else {
-	perimeter = perimeter.toFixed(2);
-}
-
-if (S === S.toFixed(2)) {
-	S = S.toFixed();
-
-} else {
-	S = S.toFixed(2);
-}
-
-if (S <= 0.00) {
-	alert('Invalid data');
-	throw 'stop';
-}
-
-
-console.log('Side C = ', sideC,'; Square = ', S,'; Perimeter = ', perimeter);
+console.log('Side C = ', roundToTwoDigitsAfterComma(sideC),'; Square = ', roundToTwoDigitsAfterComma(S),'; Perimeter = ', roundToTwoDigitsAfterComma(perimeter));
